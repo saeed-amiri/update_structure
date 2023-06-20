6cf72865ec29c3cce73ba60bda49132e9f599544
Saeed Amiri <samiri@uni-bremen.de>
2023-06-20 11:52:59 +0200

    initiate

---
+++ codes/protonating.py
@@ -0,0 +1,8 @@
+"""This script utilizes get_data.py to obtain a list and data for the
+APTES chain that requires protonation. The script then identifies
+suitable positions for each additional hydrogen atom near the N atom,
+ensuring it does not interfere with other atoms in the surrounding
+residues. Initially, the script locates a plausible area for the
+hydrogen atom and subsequently checks for the presence of other atoms.
+Finally, the script places the hydrogen atom in a suitable position.
+"""
