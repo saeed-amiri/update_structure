170fc8518bb9abbcee6af29cbcf2684df6f9deac
Saeed Amiri <samiri@uni-bremen.de>
2023-06-20 07:45:53 +0200

    initiate

---
+++ codes/get_data.py
@@ -0,0 +1,8 @@
+"""The purpose of this script is to divide the data file and extract
+the relevant section of data. It creates separate data frames for
+different residues or groups of residues. The data is accessed through
+pdb_todf.py."""
+
+
+if __name__ == '__main__':
+    pass
