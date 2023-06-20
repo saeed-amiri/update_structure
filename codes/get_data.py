e7ccf30a84541c085bfc0f583b677a09eb610bae
Saeed Amiri <samiri@uni-bremen.de>
2023-06-20 08:11:59 +0200

    get the atoms info for each residue

--- codes/get_data.py
+++ codes/get_data.py
@@ -15,13 +15,27 @@ class ProcessData:
                  fname: str  # Name of the pdb file
                  ) -> None:
         self.atoms: pd.DataFrame = pdbf.Pdb(fname).atoms
-        self.process_data()
+        self.residues_atoms: dict[str, pd.DataFrame]  # Atoms info for each res
+        self.residues_atoms = self.process_data()
 
-    def process_data(self) -> None:
+    def process_data(self) -> dict[str, pd.DataFrame]:
         """do it"""
-        residues: list[str] = self.__get_residues()
+        residues: list[str] = self.__get_residues_names()
+        residues_atoms: dict[str, pd.DataFrame] = \
+            self.__get_residues_atoms(residues)
+        return residues_atoms
 
-    def __get_residues(self) -> list[str]:
+    def __get_residues_atoms(self,
+                             residues: list[str]  # Name of the residues
+                             ) -> dict[str, pd.DataFrame]:
+        """return a dictionary of all the residues with thier atoms
+        information"""
+        residues_atoms: dict[str, pd.DataFrame] = {}  # All the atoms data
+        for res in residues:
+            residues_atoms[res] = self.atoms[self.atoms['residue_name'] == res]
+        return residues_atoms
+
+    def __get_residues_names(self) -> list[str]:
         """get the list of the residues in the system"""
         residues: list[str]   # Name of the residues
         residues = list(set(self.atoms['residue_name']))
