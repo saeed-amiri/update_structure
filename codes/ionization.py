2dc8d80c347c404553e5e015320db2dd149456ee
Saeed Amiri <samiri@uni-bremen.de>
2023-06-21 16:06:29 +0200

    get the atoms in the atoms phase

--- codes/ionization.py
+++ codes/ionization.py
@@ -6,8 +6,30 @@ protonation."""
 
 
 import sys
+import pandas as pd
 import protonating as proton
 
 
+class IonizationSol(proton.FindHPosition):
+    """ionizing the water phase of the box. The number is equal to the
+    number of deprotonation of the APTES"""
+    def __init__(self,
+                 fname: str  # Name of the pdb file
+                 ) -> None:
+        super().__init__(fname)
+        self.mk_ionization()
+
+    def mk_ionization(self) -> None:
+        """get the numbers of ions and their locations in the water
+        phase"""
+        sol_atoms: pd.DataFrame = self.__get_sol_phase_atoms()
+
+    def __get_sol_phase_atoms(self) -> pd.DataFrame:
+        """get all the atom below interface, in the water section"""
+        tresh_hold: float = \
+            self.param['INTERFACE']+100-self.param['INTERFACE_WIDTH']/2
+        return self.atoms[self.atoms['z'] < tresh_hold]
+
+
 if __name__ == '__main__':
-    prtotnation = proton.FindHPosition(sys.argv[1])
+    ioning = IonizationSol(sys.argv[1])
