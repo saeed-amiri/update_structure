6c7a84300b278c9b6e7fe0969559a7fab2a4e682
Saeed Amiri <samiri@uni-bremen.de>
2023-06-22 09:45:00 +0200

    get intervals of each smaller box, chunk, for finding space between atoms to put ions

--- codes/ionization.py
+++ codes/ionization.py
@@ -28,6 +28,38 @@ class IonizationSol(proton.FindHPosition):
         z_dims: np.ndarray  # Dimensions of the sol box in z
         sol_atoms: pd.DataFrame = self.__get_sol_phase_atoms()
         x_dims, y_dims, z_dims = self.__get_box_size(sol_atoms)
+        self.__get_chunk_interval(x_dims, y_dims, z_dims)
+
+    def __get_chunk_interval(self,
+                             x_dims: np.ndarray,  # Dimensions of sol box in x
+                             y_dims: np.ndarray,  # Dimensions of sol box in y
+                             z_dims: np.ndarray  # Dimensions of sol box in z
+                             ) -> None:
+        """get dimension of each chunk-box"""
+        x_lim: float = x_dims[1] - x_dims[0]
+        y_lim: float = y_dims[1] - y_dims[0]
+        z_lim: float = z_dims[1] - z_dims[0]
+        chunk_num: np.float64 = np.floor(np.cbrt(len(self.unprot_aptes_ind)))
+        x_chunks: list[tuple[float, float]] = \
+            self.__get_axis_chunk(x_dims, x_lim, chunk_num)  # Chunks intervals
+        y_chunks: list[tuple[float, float]] = \
+            self.__get_axis_chunk(y_dims, y_lim, chunk_num)  # Chunks intervals
+        z_chunks: list[tuple[float, float]] = \
+            self.__get_axis_chunk(z_dims, z_lim, chunk_num)  # Chunks intervals
+        print(len(x_chunks), len(y_chunks), len(z_chunks))
+
+    @staticmethod
+    def __get_axis_chunk(dims: np.ndarray,  # Dimensions of the axis
+                         lims: float,  # Limites of the box
+                         chunk_num: np.float64  # Number of chunks
+                         ) -> list[tuple[float, float]]:
+        """return the chunks of the aixs"""
+        chunk_intervals: list[tuple[float, float]] = []
+        for i in range(int(chunk_num)):
+            x_0 = dims[0]+i*lims/chunk_num
+            x_1 = x_0 + lims/chunk_num
+            chunk_intervals.append((x_0, x_1))
+        return chunk_intervals
 
     @staticmethod
     def __get_box_size(sol_atoms: pd.DataFrame  # All the atoms below NP
