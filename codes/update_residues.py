148cb9bb535cc0e177b4c8e53fa49e9864967c1e
Saeed Amiri <samiri@uni-bremen.de>
2023-06-26 11:40:09 +0200

    many changes; get new ions; update dataframes; give all the residues to one class; chack the index, to be in pdb format

--- codes/update_residues.py
+++ codes/update_residues.py
@@ -30,7 +30,7 @@ class UpdateAptesDf:
                         h_positions: dict[int, np.ndarray]  # Pos for H
                         ) -> pd.DataFrame:
         """update the aptes dataframe by adding new HN3 atoms"""
-        nh3_atoms: pd.DataFrame = self.__prepare_hydrogens(atoms, h_positions)
+        nh3_atoms: pd.DataFrame = self.prepare_hydrogens(atoms, h_positions)
         return self.__append_hydrogens(df_aptes, nh3_atoms)
 
     @staticmethod
@@ -41,9 +41,9 @@ class UpdateAptesDf:
         return pd.concat([df_aptes, hn3_atoms], ignore_index=False)
 
     @staticmethod
-    def __prepare_hydrogens(atoms: pd.DataFrame,  # All the atoms info
-                            h_positions: dict[int, np.ndarray]  # Pos for H
-                            ) -> pd.DataFrame:
+    def prepare_hydrogens(atoms: pd.DataFrame,  # All the atoms info
+                          h_positions: dict[int, np.ndarray]  # Pos for H
+                          ) -> pd.DataFrame:
         """prepare the aptes based on the structure of the main df"""
         final_atom: int = atoms.iloc[-1]['atom_id']
         cols: list[str] = ['records', 'atom_id', 'atom_name',
@@ -52,8 +52,9 @@ class UpdateAptesDf:
         hn3_atoms: pd.DataFrame = pd.DataFrame(columns=cols)
 
         for i, (key, value) in enumerate(h_positions.items()):
+            atom_id = get_pdb_index(i+1, final_atom)
             hn3_atoms.loc[i] = \
-                ['ATOM', final_atom + i + 1, 'HN3', key, 'APT', value[0],
+                ['ATOM', atom_id, 'HN3', key, 'APT', value[0],
                  value[1], value[2], 1.0, 0.0, 'H']
 
         return hn3_atoms
@@ -62,11 +63,65 @@ class UpdateAptesDf:
 class UpdateIonDf:
     """update ion dataframe by adding the prepared ions to it"""
 
-    update_ion: pd.DataFrame  # Updated ions
+    update_ions: pd.DataFrame  # Updated ions
 
     def __init__(self,
+                 atoms: pd.DataFrame,  # All the atoms
+                 ions_df: pd.DataFrame,  # Initial ions df
+                 ions_poses: np.ndarray  # Postions of the ions
                  ) -> None:
-        pass
+        self.update_ions = self.update_ion_df(atoms, ions_df, ions_poses)
+
+    def update_ion_df(self,
+                      atoms: pd.DataFrame,  # All the atoms
+                      ions_df: pd.DataFrame,  # Initial ions df
+                      ions_poses: np.ndarray  # Postions of the ions
+                      ) -> pd.DataFrame:
+        """prepare and update the ions dataframe"""
+        ions_atoms: pd.DataFrame = self.prepare_ions(atoms, ions_poses)
+        return self.__append_ions(ions_df, ions_atoms)
+
+    @staticmethod
+    def __append_ions(ions_df: pd.DataFrame,  # Initial datafrme of ions
+                      ions_atoms: pd.DataFrame  # New ions atoms
+                      ) -> pd.DataFrame:
+        """append the new ions to the old one"""
+        return pd.concat([ions_df, ions_atoms])
+
+    @staticmethod
+    def prepare_ions(atoms: pd.DataFrame,  # All the atoms
+                     ions_poses: np.ndarray  # Ions poistions
+                     ) -> None:
+        """prepare dataframe for ions"""
+        # Get the final index of the atoms after adding NH3 to the system
+        final_atom: int = atoms.iloc[-1]['atom_id'] + len(ions_poses)
+        # Get final index of the residues to add ions residues
+        final_res: int = atoms.iloc[-1]['residue_number']
+        cols: list[str] = ['records', 'atom_id', 'atom_name',
+                           'residue_number', 'residue_name', 'x', 'y', 'z',
+                           'occupancy', 'temperature', 'atom_symbol']
+        ions_atoms: pd.DataFrame = pd.DataFrame(columns=cols)
+        for i, pos in enumerate(ions_poses):
+            atom_id = get_pdb_index(i+1, final_atom)
+            res_id = get_pdb_index(i+1, final_res, pdb_max=9999)
+            ions_atoms.loc[i] = \
+                ['ATOM', atom_id, 'CLA', res_id, 'CLA', pos[0],
+                 pos[1], pos[2], 1.0, 0.0, 'CL']
+        return ions_atoms
+
+
+# Helper function to update index in pdb fasion
+def get_pdb_index(ind: int,  # Index which should be updated
+                  final_atom: int,  # Last index of atom in the system
+                  pdb_max: int = 99999  # If not atom change it
+                  ) -> int:
+    """updata index (for atoms or residues). In pdb file, atom index
+    cannot be bigger then 99999 and residues 9999. Afterwards,
+    it started from zero"""
+    new_ind: int = final_atom + ind
+    if new_ind > pdb_max:
+        new_ind -= pdb_max
+    return new_ind
 
 
 class UpdateResidues:
@@ -87,8 +142,18 @@ class UpdateResidues:
         """get all the residues"""
         self.updated_residues['APT'] = self.get_aptes(data)
         self.updated_residues['SOL'] = self.get_sol(data)
-        self.updated_residues['D10'] = self.get_sol(data)
-        self.updated_residues['COR'] = self.get_sol(data)
+        self.updated_residues['D10'] = self.get_oil(data)
+        self.updated_residues['COR'] = self.get_cor(data)
+        self.updated_residues['CLA'] = self.get_ions(data)
+
+    @staticmethod
+    def get_ions(data: ionization.IonizationSol  # All the data
+                 ) -> pd.DataFrame:
+        """get updated ions data frame"""
+        updated_ions = UpdateIonDf(data.atoms,
+                                   data.residues_atoms['CLA'],
+                                   data.ion_poses)
+        return updated_ions.update_ions
 
     @staticmethod
     def get_aptes(data: ionization.IonizationSol  # All the data
