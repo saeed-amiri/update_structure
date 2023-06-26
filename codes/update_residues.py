33e1f0e6f543c8065992637abc0b1d1e7467a24e
Saeed Amiri <samiri@uni-bremen.de>
2023-06-26 11:53:19 +0200

    returns new hn3 and ions atoms seperatly

--- codes/update_residues.py
+++ codes/update_residues.py
@@ -16,22 +16,24 @@ class UpdateAptesDf:
     positions, we have to make H atoms to be added to the pdb file"""
 
     update_aptes: pd.DataFrame  # Updated aptes chains
+    new_nh3: pd.DataFrame  # New NH3 atoms
 
     def __init__(self,
                  atoms: pd.DataFrame,  # All atoms coordinates
                  df_aptes: pd.DataFrame,  # All the APTES informations
                  h_positions: dict[int, np.ndarray]  # Index and coordinates
                  ) -> None:
-        self.update_aptes = self.update_aptes_df(atoms, df_aptes, h_positions)
+        self.update_aptes, self.new_nh3 = \
+            self.update_aptes_df(atoms, df_aptes, h_positions)
 
     def update_aptes_df(self,
                         atoms: pd.DataFrame,  # All the atoms info
                         df_aptes: pd.DataFrame,  # Aptes chains
                         h_positions: dict[int, np.ndarray]  # Pos for H
-                        ) -> pd.DataFrame:
+                        ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """update the aptes dataframe by adding new HN3 atoms"""
         nh3_atoms: pd.DataFrame = self.prepare_hydrogens(atoms, h_positions)
-        return self.__append_hydrogens(df_aptes, nh3_atoms)
+        return self.__append_hydrogens(df_aptes, nh3_atoms), nh3_atoms
 
     @staticmethod
     def __append_hydrogens(df_aptes: pd.DataFrame,  # Aptes chains
@@ -64,22 +66,24 @@ class UpdateIonDf:
     """update ion dataframe by adding the prepared ions to it"""
 
     update_ions: pd.DataFrame  # Updated ions
+    new_ions: pd.DataFrame  # New ions
 
     def __init__(self,
                  atoms: pd.DataFrame,  # All the atoms
                  ions_df: pd.DataFrame,  # Initial ions df
                  ions_poses: np.ndarray  # Postions of the ions
                  ) -> None:
-        self.update_ions = self.update_ion_df(atoms, ions_df, ions_poses)
+        self.update_ions, self.new_ions = \
+            self.update_ion_df(atoms, ions_df, ions_poses)
 
     def update_ion_df(self,
                       atoms: pd.DataFrame,  # All the atoms
                       ions_df: pd.DataFrame,  # Initial ions df
                       ions_poses: np.ndarray  # Postions of the ions
-                      ) -> pd.DataFrame:
+                      ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """prepare and update the ions dataframe"""
         ions_atoms: pd.DataFrame = self.prepare_ions(atoms, ions_poses)
-        return self.__append_ions(ions_df, ions_atoms)
+        return self.__append_ions(ions_df, ions_atoms), ions_atoms
 
     @staticmethod
     def __append_ions(ions_df: pd.DataFrame,  # Initial datafrme of ions
