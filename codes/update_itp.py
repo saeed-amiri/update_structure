b51ade26e3b8a5f0b84860d3ec5d93c639fbd2d5
Saeed Amiri <samiri@uni-bremen.de>
2023-06-28 08:30:02 +0200

    fix type of the class

--- codes/update_itp.py
+++ codes/update_itp.py
@@ -121,8 +121,8 @@ class UpdateAtom:
                 df_info.loc[df_info['atomname'] == key, item].values[0]
         return columns, info
 
-    def __get_indices(self,
-                      atoms: pd.DataFrame,  # Atoms of the itp file
+    @staticmethod
+    def __get_indices(atoms: pd.DataFrame,  # Atoms of the itp file
                       hn3: pd.DataFrame  # New HN3 to add to the atoms
                       ) -> np.int64:
         """return the maximum value of the atoms and check mismatch
@@ -138,7 +138,7 @@ class UpdateAtom:
         lst_nh3_res: np.int64  # index of the final residue
         lst_nh3_res = list(hn3['residue_number'])[-1]
         if np.max(np.array([max_resnr, lst_resnr])) != lst_nh3_res:
-            sys.exit(f'{bcolors.FAIL}{self.__module__}:\n'
+            sys.exit(f'{bcolors.FAIL}{UpdateAtom.__module__}:\n'
                      '\tThere is mismatch in the new HN3 and initial'
                      f' APTES list.\n{bcolors.ENDC}')
         return np.max(np.array([max_atomnr, lst_atomnr]))
