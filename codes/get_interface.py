f1880720c6afd29298eed37606017e61c4e29bc7
Saeed Amiri <samiri@uni-bremen.de>
2023-06-30 13:40:13 +0200

    getting surface level from the current data

---
+++ codes/get_interface.py
@@ -0,0 +1,5 @@
+"""To determine the level of APTES protonation, locating the water's
+surface is necessary. This requires inputting all nanoparticle data
+(COR and APT) for accurate placement. As the unwrap structure cannot
+be used, the script identifies the location of each water molecule
+based on the location of the O atom in the water molecules."""
