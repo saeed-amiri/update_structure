da677e454a27f998ddf52c80525f221a839e933e
Saeed Amiri <samiri@uni-bremen.de>
2023-07-11 08:09:47 +0200

    update doc

--- codes/read_param.py
+++ codes/read_param.py
@@ -2,17 +2,6 @@
 in this file # is comment symbol
 the equal (=) sign is used for geting the value for the key
 All the info must be in capital
-This file must include:
-    ############### CONSTANT VALUES ##############
-    # The contact angle of the nanoparticle (NP)
-    ANGLE=90
-    # Estimate radius of the NP before functionalization with APTES
-    RADIUS=25
-    ############### INTERACTIVE VALUES ###########
-    # intereface location -> caculated by analysing script
-    INTERFACE=12
-    # interface thickness
-    INTERFACE_WIDTH=10
 """
 
 import sys
