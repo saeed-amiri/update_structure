661cb51c59677bbe07768246c3193808e6714d61
Saeed Amiri <samiri@uni-bremen.de>
2023-07-31 09:07:31 +0200

    make the total names of the itp files

--- codes/read_param.py
+++ codes/read_param.py
@@ -174,7 +174,7 @@ class ReadParam:
             itp = itp.strip()
             itps = my_tools.drop_string(itp, '[', ']')
             names = itps.split(',')
-            files_list.append(names[0])
+            files_list.append(names[0].__add__('.itp'))
             aptes_list.append(names[1])
             cores_list.append(names[2])
         self.param['itp_files'] = files_list
