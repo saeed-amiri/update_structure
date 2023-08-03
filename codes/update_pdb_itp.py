6a919fa334320705f408c0fa322f126bfc2f8c20
Saeed Amiri <samiri@uni-bremen.de>
2023-08-03 15:25:44 +0200

    updates!

--- codes/update_pdb_itp.py
+++ codes/update_pdb_itp.py
@@ -13,6 +13,7 @@ the NP's location.
 
 
 import sys
+import pandas as pd
 import update_residues_pdb
 import update_residues_gro
 import write_pdb_file
@@ -26,11 +27,12 @@ if __name__ == '__main__':
     if STYLE == 'GRO':
         gro_data = update_residues_gro.UpdateResidues(sys.argv[1])
         write_gro_file.write_gromacs_gro(gro_data, 'updated_system.gro')
-        new_hn3 = gro_data.new_hn3
+        new_hn3_dict: dict[str, pd.DataFrame] = gro_data.new_hn3
     else:
-        sys.exit('\nDEPRECTED!CANNOT RUN READ THIS! USE GRO FILES\n')
+        sys.exit('\nDEPRECTED! CANNOT READ THIS! USE GRO FILES\n')
         up_data = update_residues_pdb.UpdateResidues(sys.argv[1])
         write_pdb_file.WritePdb(up_data)
         new_hn3 = up_data.new_hn3
-    itp = update_itp.UpdateItp(fname='APT_COR.itp', hn3=new_hn3)
-    write_itp_file.WriteItp(itp, fname='APT_COR_updated.itp')
+    itp = update_itp.WrapperUpdateItp(
+        param=gro_data.param, hn3_dict=new_hn3_dict)
+    write_itp_file.WrapperWriteItp(itp)
