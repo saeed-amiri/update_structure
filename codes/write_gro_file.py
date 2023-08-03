86f885de55c3d9c1e9d71cd981a08ad07582ba8e
Saeed Amiri <samiri@uni-bremen.de>
2023-08-03 16:41:01 +0200

    pylint

--- codes/write_gro_file.py
+++ codes/write_gro_file.py
@@ -9,7 +9,7 @@ def write_gromacs_gro(gro_data: update_residues_gro.UpdateResidues,
                       ) -> None:
     """Write DataFrame to a GROMACS gro file."""
     df_i: pd.DataFrame = gro_data.combine_residues
-    with open(filename, 'w') as gro_file:
+    with open(filename, 'w', encoding='utf8') as gro_file:
         gro_file.write(f'{gro_data.title}\n')  # Add a comment line
         gro_file.write(f'{len(df_i)}\n')  # Write the total number of atoms
         for _, row in df_i.iterrows():
