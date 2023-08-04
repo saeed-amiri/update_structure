d60c81f5f64f9af3636fe2b10e3f2c5c5d088b11
Saeed Amiri <samiri@uni-bremen.de>
2023-08-04 10:44:11 +0200

    update to use for debugging

--- codes/write_gro_file.py
+++ codes/write_gro_file.py
@@ -4,13 +4,16 @@ import pandas as pd
 import update_residues_gro
 
 
-def write_gromacs_gro(gro_data: update_residues_gro.UpdateResidues,
-                      filename: str  # Name of the output file
+def write_gromacs_gro(gro_data: pd.DataFrame,
+                      filename: str,  # Name of the output file
+                      pbc_box=None,
+                      title=None
                       ) -> None:
     """Write DataFrame to a GROMACS gro file."""
-    df_i: pd.DataFrame = gro_data.combine_residues
+    df_i: pd.DataFrame = gro_data.copy()
     with open(filename, 'w', encoding='utf8') as gro_file:
-        gro_file.write(f'{gro_data.title}\n')  # Add a comment line
+        if title:
+            gro_file.write(f'{title}\n')  # Add a comment line
         gro_file.write(f'{len(df_i)}\n')  # Write the total number of atoms
         for _, row in df_i.iterrows():
             line = f'{row["residue_number"]:>5}' \
@@ -24,4 +27,5 @@ def write_gromacs_gro(gro_data: update_residues_gro.UpdateResidues,
                    f'{row["vy"]:8.4f}' \
                    f'{row["vz"]:8.4f}\n'
             gro_file.write(line)
-        gro_file.write(f'{gro_data.pbc_box}\n')
+        if pbc_box:
+            gro_file.write(f'{pbc_box}\n')
