31c775aa5de5a386be3801508db7b9bd01ef3817
Saeed Amiri <samiri@uni-bremen.de>
2023-08-18 11:57:54 +0200

    get number of cores

--- codes/cpuconfig.py
+++ codes/cpuconfig.py
@@ -4,18 +4,25 @@ that can be used.
 """
 
 import socket
+import multiprocessing
 
 class ConfigCpuNr:
     """
     Find the number of core
     """
     def __init__(self) -> None:
-        self.get_hostname()
+        self.hostname: str = self.get_hostname()
+        self.core_nr: int = self.get_core_nr()
 
     def get_hostname(self) -> str:
         """find the name of the host"""
         hostname = socket.gethostname()
         print(hostname)
+
+    def get_core_nr(self) -> int:
+        """return numbers of cores"""
+        core_nr: int = multiprocessing.cpu_count()
+        print(core_nr)
 
 
 if __name__ == '__main__':
