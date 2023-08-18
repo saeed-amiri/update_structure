c87fa311831ec3c6100cfa265edb1e49706bec2d
Saeed Amiri <samiri@uni-bremen.de>
2023-08-18 13:05:15 +0200

    update

--- codes/cpuconfig.py
+++ codes/cpuconfig.py
@@ -1,7 +1,94 @@
 """
-Find where the script is running and set the number of available cores
-that can be used.
+ConfigCpuNr Module Documentation
+-------------------------------
+
+The `ConfigCpuNr` module provides a class, `ConfigCpuNr`, that
+facilitates the configuration of the number of available CPU cores
+based on the host environment where a script is running. This module
+is particularly useful for optimizing multi-core computation by
+adapting the core utilization to different computing environments.
+
+Module Overview:
+-----------------
+
+The `ConfigCpuNr` class in this module allows for dynamic determination
+of the number of CPU cores that should be utilized for a computational
+task. By considering the hostname and the type of computing environment,
+the class can intelligently adjust the core count to achieve efficient
+resource utilization.
+
+Class Details:
+--------------
+
+class ConfigCpuNr:
+    ConfigCpuNr class is designed to determine the optimal number of
+    CPU cores to use for a computation based on the host environment.
+
+    Attributes:
+        hostname (str): The hostname of the machine where the script
+        is running.
+        core_nr (int): The configured number of CPU cores for the
+        computation.
+
+    Methods:
+        __init__(self, log: logger.logging.Logger)
+            Initializes the ConfigCpuNr instance and configures the
+            number of available CPU cores based on the host environment.
+
+        set_core_numbers(self) -> int
+            Determines the appropriate number of CPU cores based on
+            the hostname and specific rules for different computing
+            environments.
+
+        get_hostname(self) -> str
+            Retrieves and returns the hostname of the machine.
+
+        get_core_nr(self) -> int
+            Retrieves the total number of available CPU cores.
+
+        write_log_msg(self, log: logger.logging.Logger) -> None
+            Logs informative messages about core configuration to the
+            provided logger instance and prints the messages to the
+            console.
+
+    Usage Example:
+    --------------
+
+    from ConfigCpuNr import ConfigCpuNr
+    import logger
+
+    if __name__ == '__main__':
+        # Create a logger instance
+        log = logger.setup_logger(log_name='ConfigeCpu.log')
+
+        # Initialize ConfigCpuNr and set the number of CPU cores
+        cpu_config = ConfigCpuNr(log=log)
+
+        # Access the configured core count
+        num_cores = cpu_config.core_nr
+        print(f"Number of cores configured for computation: {num_cores}")
+
+Module Dependencies:
+--------------------
+
+- `socket`: Provides access to low-level networking interfaces.
+- `multiprocessing`: Enables multi-core processing and CPU count
+    retrieval.
+- `logger`: An external logger module for logging purposes.
+- `colors_text`: An external module for text color formatting (not
+    provided).
+
+Notes:
+------
+
+- To use this module effectively, ensure that the necessary dependencies
+    are available.
+- This module makes use of a custom logger module (not provided) and a
+text color formatting module (not provided) for enhanced logging and
+console output.
+
 """
+
 
 import socket
 import multiprocessing
@@ -46,9 +133,12 @@ class ConfigCpuNr:
         return core_nr
 
     def get_hostname(self) -> str:
-        """find the name of the host"""
-        hostname = socket.gethostname()
-        self.info_msg += f'\t\tHostname is `{hostname}`\n'
+        """Retrieve the hostname of the machine."""
+        try:
+            hostname = socket.gethostname()
+            self.info_msg += f'\t\tHostname is `{hostname}`\n'
+        except socket.error as err:
+            raise RuntimeError("Failed to retrieve hostname.") from err
         return hostname
 
     def get_core_nr(self) -> int:
