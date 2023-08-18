fc975b3d1637d301b07a67f2b7bb8815e7d8caef
Saeed Amiri <samiri@uni-bremen.de>
2023-08-18 12:14:42 +0200

    get nrs, log

--- codes/cpuconfig.py
+++ codes/cpuconfig.py
@@ -5,25 +5,43 @@ that can be used.
 
 import socket
 import multiprocessing
+import logger
+from colors_text import TextColor as bcolors
+
 
 class ConfigCpuNr:
     """
     Find the number of core
     """
-    def __init__(self) -> None:
+
+    info_msg: str = 'message from ConfigCpuNr:\n' # Meesage from methods to log
+
+    def __init__(self,
+                 log: logger.logging.Logger
+                 ) -> None:
         self.hostname: str = self.get_hostname()
         self.core_nr: int = self.get_core_nr()
+        self.write_log_msg(log)
 
     def get_hostname(self) -> str:
         """find the name of the host"""
         hostname = socket.gethostname()
-        print(hostname)
+        self.info_msg += f'\t\tHostname is `{hostname}`\n'
+        return hostname
 
     def get_core_nr(self) -> int:
         """return numbers of cores"""
         core_nr: int = multiprocessing.cpu_count()
-        print(core_nr)
+        self.info_msg += f'\t\tNumber of cores is: `{core_nr}`\n'
+        return core_nr
 
+    def write_log_msg(self,
+                      log: logger.logging.Logger  # Name of the output file
+                      ) -> None:
+        """writing and logging messages from methods"""
+        log.info(self.info_msg)
+        print(f'{bcolors.OKBLUE}{self.__module__}:\n'
+              f'\t{self.info_msg}\n{bcolors.ENDC}')
 
 if __name__ == '__main__':
-    ConfigCpuNr()
+    ConfigCpuNr(log=logger.setup_logger(log_name='ConfigeCpu.log'))
