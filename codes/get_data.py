627dc936094882f53ea19322c12430cb152170e0
Saeed Amiri <samiri@uni-bremen.de>
2023-10-02 16:38:56 +0200

    update info msg

--- codes/get_data.py
+++ codes/get_data.py
@@ -172,7 +172,7 @@ class ProcessData:
             self.find_unprotonated_aptes(log)
         if self.param['NUMAPTES'] != -1:
             self.info_msg += ('\tThe number of unprotonated aptes is set'
-                              f' to {self.param["NUMAPTES"]}\n')
+                              f' to {int(self.param["NUMAPTES"])}\n')
         self.unproton_aptes, self.unprot_aptes_ind = \
             self.select_lowest_aptes(unproton_aptes)
         # Get the diameter of the NP
